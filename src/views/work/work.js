f4a2d5484cc1366bfdec68bed120e6bdca86fca3
gcheng-appen <gcheng@appen.com>
2017-09-28 12:32:30 +1000

    improved work GUI to merge headwords

--- src/views/work/work.js
+++ src/views/work/work.js
@@ -2,23 +2,66 @@ var mod = angular.module('at.views.work', []);
 
 mod.controller('WorkCtrl', function ($scope, batchId, $q, Lex, $filter, $log) {
 
+    var validators = [],
+        fullAlphabets = {};
+
+    function parseAlphabet(alphabet) {
+        $log.debug('loaded alphabet', alphabet);
+        fullAlphabets[alphabet.alphabetId] = alphabet;
+        alphabet.validators = [];
+        angular.forEach(alphabet.phonologyRules, function (rule) {
+            $log.debug(rule);
+        });
+        angular.forEach(alphabet.stressRules, function (rule) {
+            $log.debug(rule);
+        });
+        angular.forEach(alphabet.syllabificationRules, function (rule) {
+            $log.debug(rule);
+        });
+        angular.forEach(alphabet.vowelisationRules, function (rule) {
+            $log.debug(rule);
+        });
+    }
+
     (function init(delay) {
         $scope.context = {isLoading: true};
         $scope.batchId = batchId;
         $scope.items = [];
         $scope.currentItem = null;
+        $scope.alphabets = [];
 
         $q.all({
-            batch: Lex.getBatch(batchId)
+            batch: Lex.getBatch(batchId),
+            alphabets: Lex.getAlphabets()
         }).then(function (result) {
             var batch = result.batch.data.batch,
-                items = $filter('orderBy')(batch.data, ['headword', 'ranking']);
+                items = $filter('orderBy')(batch.data, ['headword', 'ranking']),
+                itemByHeadword = {};
 
-            $scope.batch = batch;
-            angular.forEach(items, function (item, i) {
-                item.index = i;
-                item.spellingCorrection = item.headword;
-                $scope.items.push(item);
+            $scope.alphabets = result.alphabets.data.alphabets;
+
+            angular.forEach(items, function (item) {
+                var hw = item.headword, newItem;
+                if (!angular.isDefined(itemByHeadword[hw])) {
+                    newItem = {
+                        index: $scope.items.length,
+                        headword: hw,
+                        spellingCorrection: hw,
+                        variants: [
+                            {
+                                sampa: item.sampa,
+                                tag: item.tag
+                            }
+                        ]
+                    };
+                    itemByHeadword[hw] = newItem;
+                    $scope.items.push(newItem);
+                } else {
+                    itemByHeadword[hw].variants.push({
+                        sampa: item.sampa,
+                        tag: item.tag
+                    });
+                }
             });
             if (items.length) {
                 $scope.currentItem = $scope.items[0];
@@ -28,6 +71,30 @@ mod.controller('WorkCtrl', function ($scope, batchId, $q, Lex, $filter, $log) {
         });
     })(0);
 
+    $scope.$watch('currentItem', function (item) {
+        $scope.formInput = {};
+        if (item) {
+            $scope.formInput.spellingCorrection = item.spellingCorrection;
+            $scope.formInput.sampa = item.sampa;
+            $scope.formInput.tag = item.tag;
+            $scope.formInput.variants = item.variants.slice()
+        }
+    });
+    $scope.$watch('alphabetId', function (alphabetId) {
+        validators.length = 0;
+        if (angular.isNumber(alphabetId)) {
+            $log.debug('chosen alphabet', alphabetId);
+            if (angular.isDefined(fullAlphabets[alphabetId])) {
+                validators.concat(fullAlphabets[alphabetId].validators)
+            } else {
+                Lex.getAlphabet(alphabetId).then(function (response) {
+                    var alphabet = response.data.alphabet;
+                    parseAlphabet(alphabet);
+                    validators.concat(alphabet.validators);
+                });
+            }
+        }
+    });
     $scope.getItemsBehind = function () {
         var rs = [];
         if ($scope.currentItem) {
@@ -49,14 +116,6 @@ mod.controller('WorkCtrl', function ($scope, batchId, $q, Lex, $filter, $log) {
     $scope.setCurrentItem = function (item) {
         $scope.currentItem = item;
     };
-    $scope.$watch('currentItem', function (item) {
-        $scope.formInput = {};
-        if (item) {
-            $scope.formInput.spellingCorrection = item.spellingCorrection;
-            $scope.formInput.sampa = item.sampa;
-            $scope.formInput.tag = item.tag;
-        }
-    });
     $scope.save = function () {
         $log.debug('save current edited item, spellingCorrection, sampa, tag, etc.');
     };
